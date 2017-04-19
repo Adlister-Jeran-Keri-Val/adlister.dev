610ac5ee39715a383fb6fdeeac8f84278aefa1f8
Keri C <Kerington@Keris-MacBook-Pro.local>
2017-04-19 09:05:07 -0500

    Adding updated CSS and HTML files.

--- public/index.php
+++ public/index.php
@@ -9,12 +9,13 @@ require_once __DIR__ . '/../bootstrap.php';
     <title>ShoeLaLa</title>
     <?php require '../views/partials/head.php'; ?>
     <link rel="stylesheet" href="/css/main.css">
-    <link href="https://fonts.googleapis.com/css?family=BenchNine:300|Satisfy" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=BenchNine:300|Yanone+Kaffeesatz:200|Satisfy" rel="stylesheet">
 </head>
 <body>
 <h1></h1>
     <?php require '../views/partials/navbar.php'; ?>
     <?php require $mainView; ?>
     <?php require '../views/partials/common_js.php'; ?>
+    <?php require '../views/partials/footer.php' ; ?>
 </body>
 </html>
