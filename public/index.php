eae8b0ac9b65db909ef6a160d7830d215d5fd1e2
Keri C <Kerington@Keris-MacBook-Pro.local>
2017-04-21 13:59:57 -0500

    Updating HTML and CSS

--- public/index.php
+++ public/index.php
@@ -11,7 +11,7 @@ require_once __DIR__ . '/../bootstrap.php';
     <title>ShoeLaLa</title>
     <!-- CUSTOM CSS -->
     <link rel="stylesheet" href="/css/main.css">
-    <link href="https://fonts.googleapis.com/css?family=BenchNine:300|Yanone+Kaffeesatz:200|Satisfy" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=BenchNine|Yanone+Kaffeesatz:200|Satisfy" rel="stylesheet">
 </head>
 <body>
 <h1></h1>
--- views/ads/show.php
+++ views/ads/show.php
@@ -9,17 +9,24 @@ $shoe = Shoe::find($_GET['id']);
 
 
 <div class='shoe_box'>
-	<h2 class="shoe_title"><?= $shoe->title ?></h2>
-	<center><img src="<?= $shoe->image_location?>" height="350px" width="350px"></center>
-	<p class="show_shoe_price">ASKING: $<?= $shoe->price ?></p>
-	<p class="show_shoe_location">MSRP: <?= $shoe->msrp ?></p>
-	<p class="show_shoe_location">COLOR: <?= $shoe->color ?></p>
-	<p class="show_shoe_brand">BRAND: <?= $shoe->brand ?></p>
-	<p class="show_shoe_size"=>SIZE: <?= $shoe->size ?></p>
-	<p class="show_shoe_location">CONDITION: <?= $shoe->shoe_condition ?></p>
-	<p class="show_shoe_brand">CONTACT NAME: <?= $shoe->contact_name ?></p>
-	<p class="show_shoe_location">CONTACT INFO: <?= $shoe->contact_number?></p>
-	<p class="show_shoe_size"=>LOCATED: <?= $shoe->location ?></p>
-	<p class="show_shoe_location">DESCRIPTION: <br> <?= $shoe->item_description ?></p>
+	<div class="col-xs-4 col-sm-4 col-md-4 col-lg-4 col-xs-offset-1" id="left_side">
+		<h2 class="shoe_title"><?= $shoe->title ?></h2>
+		<img src="<?= $shoe->image_location?>" height="350px" width="350px">
+	</div>
+	<div class="col-xs-3 col-sm-3 col-md-3 col-lg-3" id="middle">
+		<p class="show_shoe_price"><u>ASKING:</u> $<?= $shoe->price ?></p>
+		<p class="show_shoe_msrp"><u>MSRP:</u> $<?= $shoe->msrp ?></p>
+		<p class="show_shoe_color"><u>COLOR:</u> <?= $shoe->color ?></p>
+		<p class="show_shoe_brand"><u>BRAND:</u> <?= $shoe->brand ?></p>
+		<p class="show_shoe_size"=><u>SIZE:</u> <?= $shoe->size ?></p>
+		<p class="show_shoe_condition"><u>CONDITION:</u> <?= $shoe->shoe_condition ?></p>
+	</div>
+	<div class="col-xs-4 col-sm-4 col-md-4 col-lg-4" id="right_side">
+		<p class="show_shoe_contact_name"><u>CONTACT NAME:</u> <?= $shoe->contact_name ?></p>
+		<p class="show_shoe_contact_info"><u>CONTACT INFO:</u> <?= $shoe->contact_number?></p>
+		<p class="show_shoe_location"=><u>LOCATED:</u> <?= $shoe->location ?></p>
+		<p class="show_shoe_description"><u>DESCRIPTION:</u> <br> <?= $shoe->item_description ?></p>
+		<a href="/editshoe" class="log_in_button"> CLICK HERE TO EDIT YOUR LISTING </a>
+	</div>
 </div>
-<a href="/editshoe" class="log_in_button"> CLICK HERE TO EDIT YOUR LISTING </a>
+<br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
