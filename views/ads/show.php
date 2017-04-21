432586d20e081d504d2930fba1fe1477c20d3e16
Valerie Burk <valerieburk@aim.com>
2017-04-21 11:03:43 -0500

    Adding CSS changes, login bar

--- views/ads/show.php
+++ views/ads/show.php
@@ -12,8 +12,14 @@ $shoe = Shoe::find($_GET['id']);
 	<h2 class="shoe_title"><?= $shoe->title ?></h2>
 	<center><img src="<?= $shoe->image_location?>" height="350px" width="350px"></center>
 	<p class="show_shoe_price">ASKING: $<?= $shoe->price ?></p>
-	<p class="show_shoe_location">LOCATED: <?= $shoe->location ?></p>
+	<p class="show_shoe_location">MSRP: <?= $shoe->msrp ?></p>
+	<p class="show_shoe_location">COLOR: <?= $shoe->color ?></p>
 	<p class="show_shoe_brand">BRAND: <?= $shoe->brand ?></p>
 	<p class="show_shoe_size"=>SIZE: <?= $shoe->size ?></p>
+	<p class="show_shoe_location">CONDITION: <?= $shoe->shoe_condition ?></p>
+	<p class="show_shoe_brand">CONTACT NAME: <?= $shoe->contact_name ?></p>
+	<p class="show_shoe_location">CONTACT INFO: <?= $shoe->contact_number?></p>
+	<p class="show_shoe_size"=>LOCATED: <?= $shoe->location ?></p>
+	<p class="show_shoe_location">DESCRIPTION: <br> <?= $shoe->item_description ?></p>
 </div>
 <a href="/editshoe" class="log_in_button"> CLICK HERE TO EDIT YOUR LISTING </a>
--- views/users/account.php
+++ views/users/account.php
@@ -1,12 +1,17 @@
 <!--Page for user account home-->
 <?php
 require_once __DIR__ . '/../../models/Shoes.php';
+require_once __DIR__ . '/../../models/User.php';
 
 if (!Auth::check()){
 	header('location: /login');
 	die;
 }
+
+$user = User::find($_GET['id']);
+
+
 ?>
 
 
-<h3> Welcome</h3>
+<h2> Welcome <?= $user->name ?></h2>
